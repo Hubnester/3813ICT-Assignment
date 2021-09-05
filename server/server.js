2f4bf3d8a57530f43685f123f95842f7ae024af1
Hubnester <jamesdolphin001@gmail.com>
2021-09-05 21:19:25 +1000

    Create Node Server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+
+var http = require("http").Server(app);
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var cors = require("cors");
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "../dist/assignment")));
+
+let server = http.listen(3000, function(){
+	let host = server.address().address;
+	let port = server.address().port;
+	console.log("Server listening on: " + host + " port: " + port);
+});
